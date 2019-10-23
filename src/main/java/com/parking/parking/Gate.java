1326021f202d27f5d547fe8127a33d29194a6b82
jillian <jillian.fredette@gmail.com>
2019-10-23 10:39:33 -0500

    Gate updated

--- src/main/java/com/parking/parking/Gate.java
+++ src/main/java/com/parking/parking/Gate.java
@@ -2,14 +2,11 @@ package com.parking.parking;
 
 public abstract class Gate{
     //a gate is either entrance or exit
-    String type;
+    GateType type;
     //a gate has to belong to a lot
     ParkingLot lot;
-    //a gate has a number
-    int gateNum;
 
-    public Gate(ParkingLot l, int n) {
+    public Gate(ParkingLot l) {
         lot = l;
-        gateNum = n;
     }
 }
