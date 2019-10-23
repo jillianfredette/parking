b79206768e72bb429016de362b21a7c62d6b5a3f
jillian <jillian.fredette@gmail.com>
2019-10-23 10:40:37 -0500

    Exit Gate Updated

--- src/main/java/com/parking/parking/ExitGate.java
+++ src/main/java/com/parking/parking/ExitGate.java
@@ -2,17 +2,13 @@ package com.parking.parking;
 
 public class ExitGate extends Gate {
 
-    public ExitGate(ParkingLot l, int n){
-        super(l, n);
+    public ExitGate(ParkingLot l){
+        super(l);
         //exit gate
-        type = "Exit";
+        type = GateType.Exit;
     }
 
     public boolean ExitLot(Car c){
-        if(c.ticket == null || c.ticket.lot != lot){
-            System.out.println("Car "+c.license+" is not in Lot "+ lot.name);
-            return false;
-        }
         Ticket ticket = c.giveTicket();
         System.out.println("Car "+c.license+" presents ticket with timestamp "+ticket.timestamp);
         lot.payTicket(ticket.price);
