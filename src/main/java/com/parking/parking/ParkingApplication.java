507d65e4389e695a55458480d4e19edc7a7839d3
jillian <jillian.fredette@gmail.com>
2019-10-23 12:06:06 -0500

    Updates to Parking Application

--- src/main/java/com/parking/parking/ParkingApplication.java
+++ src/main/java/com/parking/parking/ParkingApplication.java
@@ -19,35 +19,70 @@ public class ParkingApplication {
 		File file = new File(args[0]);
 		BufferedReader reader = new BufferedReader(new FileReader(file));
 
+		HashMap<String, ParkingGroup> groups = new HashMap<String, ParkingGroup>();
 		HashMap<String, ParkingLot> parkingLots = new HashMap<String, ParkingLot>();
-		HashMap<Integer, Car> cars = new HashMap<Integer, Car>();
+		HashMap<String, Car> cars = new HashMap<String, Car>();
 
 		//read in each line
 		String curr;
 
 		try {
+			//get Parking Groups
+			if ((curr = reader.readLine()).equals("Parking Groups")){
+				while((curr = reader.readLine()).length() > 0){
+					String[] group = curr.split(" ");
+					if(group.length > 3) throw new Exception();
+
+					//create parking group
+					if(groups.containsKey(group[0])){
+						System.out.println("Group "+group[0]+" already exists");
+						throw new Exception();
+					}
+					String newgroup = group[0];
+					int price = Integer.parseInt(group[1]);
+					int discount = Integer.parseInt(group[2]);
+					//check for correct input
+					if(price < 0){
+						System.out.println("Price cannot be negative");
+						throw new Exception();
+					}
+					if(discount < 0){
+						System.out.println("Discount cannot be negative");
+						throw new Exception();
+					}
+					if(discount > 100){
+						System.out.println("discount cannot be more than 100 percent");
+						throw new Exception();
+					}
+					groups.put(newgroup, new ParkingGroup(newgroup, price, discount));
+				}
+				System.out.println("");
+			}
+			else throw new Exception();
+
 			//get Parking Lots
 			if ((curr = reader.readLine()).equals("Parking Lots")) {
 				while((curr = reader.readLine()).length() > 0){
 					String[] lot = curr.split(" ");
+					if (lot.length > 3) throw new Exception();
+					String newlot = lot[0];
+					String group = lot[1];
+					int capacity = Integer.parseInt(lot[2]);
 
 					//create parking lot
 					if(parkingLots.containsKey(lot[0])){
-						System.out.println("Lot "+lot[0]+" already exists");
+						System.out.println("Lot "+newlot+" already exists");
 						throw new Exception();
 					}
-					parkingLots.put(lot[0], new ParkingLot(lot[0], Integer.parseInt(lot[1]), Integer.parseInt(lot[2])));
-
-					//add entrance gates
-					for(int i = 1; i <= Integer.parseInt(lot[3]); i++){
-						parkingLots.get(lot[0]).addEntrance(i);
+					if(!groups.containsKey(lot[1])){
+						System.out.println("Group "+group+" does not exist");
+						throw new Exception();
 					}
-
-					//add exit gates
-					for(int i = 1; i <= Integer.parseInt(lot[4]); i++){
-						parkingLots.get(lot[0]).addExit(i+Integer.parseInt(lot[3]));
+					if(capacity < 0){
+						System.out.println("Capacity of a lot cannot be negative");
+						throw new Exception();
 					}
-					System.out.println("");
+					parkingLots.put(newlot, new ParkingLot(groups.get(group), newlot, capacity));
 				}
 				System.out.println("");
 			}
@@ -57,38 +92,50 @@ public class ParkingApplication {
 			if ((curr = reader.readLine()).equals("Cars")) {
 				while((curr = reader.readLine()).length() > 0){
 					//add car
-					if(cars.containsKey(Integer.parseInt(curr))){
+					if(cars.containsKey(curr)){
 						System.out.println("Car "+curr+" already exists");
 						throw new Exception();
 					}
-					cars.put(Integer.parseInt(curr), new Car(Integer.parseInt(curr)));
+					cars.put(curr, new Car(curr));
 				}
-				System.out.println("\n");
+				System.out.println("");
 			}
 			else throw new Exception();
 
-			//process Actions
 			if ((curr = reader.readLine()).equals("Actions")) {
 				while((curr = reader.readLine()) != null && curr.length() > 0){
 					//process action
 					String[] action = curr.split(" ");
-					int car = Integer.parseInt(action[0]);
+					String car = action[0];
 					String act = action[1];
-					String lot = action[2];
-					int gate = Integer.parseInt(action[3]);
-					if(act.equals("Enter")){
-						if(!parkingLots.get(lot).Enter(cars.get(car), gate)){
-							System.out.println("Car "+ car+" is unable to enter Lot "+lot+" throw gate "+gate);
-						}
+
+					//if park or exit
+					if(action.length == 2){
+						if (act.equals("Park")) cars.get(car).park(parkingLots);
+						else if(act.equals("Exit")) cars.get(car).exit();
+						else throw new Exception();
 					}
-					else if(act.equals("Exit")){
-						if(!parkingLots.get(lot).Exit(cars.get(car), gate)){
-							System.out.println("Car "+ car+" is unable to exit Lot "+lot+" throw gate "+gate);
+					//if inquire
+					else if( action.length == 4){
+						if(!act.equals("Inquire")) throw new Exception();
+						String actTwo = action[2];
+						//price or space inquiry
+						if(actTwo.equals("Price")){
+							String group = action[3];
+							cars.get(car).inquirePrice(groups.get(group));
 						}
+						else if (actTwo.equals("Space")){
+							String lot = action[3];
+							cars.get(car).inquireSpace(parkingLots.get(lot));
+						}
+						else throw new Exception();
+
 					}
+					//else, error in input file
 					else throw new Exception();
+					System.out.println("");
 				}
-				System.out.println("\n");
+				System.out.println("");
 			}
 			else throw new Exception();
 
