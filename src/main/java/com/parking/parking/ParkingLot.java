a5d81d8d30ede6a3819f0b1cd4aa57543961a151
jillian <jillian.fredette@gmail.com>
2019-10-23 10:41:31 -0500

    Parking Lot updated

--- src/main/java/com/parking/parking/ParkingLot.java
+++ src/main/java/com/parking/parking/ParkingLot.java
@@ -8,60 +8,35 @@ public class ParkingLot {
     final int capacity;
     //current occupancy
     int occupancy;
-    //price of the lot
-    final int price;
     //current profits
     int profit;
     String name;
-    HashMap<Integer, EntranceGate> entGates = new HashMap<Integer, EntranceGate>();
-    HashMap<Integer, ExitGate> exitGates = new HashMap<Integer, ExitGate>();
+    ParkingGroup group;
+    EntranceGate entGate;
+    ExitGate exitGate;
 
-    public ParkingLot(String n, int c, int p){
+    public ParkingLot(ParkingGroup g, String n, int c){
+        //set group lot belongs to
+        group = g;
         //initialized with a max capacity
         capacity = c;
         //initially empty
         occupancy = 0;
-        //initialize with the price of the lot
-        price = p;
         //initially has no profits
         profit = 0;
+        //give lot a name
         name = n;
-        System.out.println("Parking Lot "+this.name+" added with capacity "+ this.capacity +" cars and price "+this.price+" cents");
+        entGate = new EntranceGate(this);
+        exitGate = new ExitGate(this);
+        System.out.println("Parking Lot "+this.name+" added with a capacity of "+ this.capacity +" cars and belongs to Parking Group "+group.name);
     }
 
-    public boolean addEntrance(int n){
-        if(entGates.containsKey(n)){
-            return false;
-        }
-
-        entGates.put(n, new EntranceGate(this, n));
-        System.out.println("Gate "+n+" added to Lot "+this.name+" as an Entrance");
-
-        return true;
-    }
-
-    public boolean addExit(int n){
-        if(exitGates.containsKey(n)){
-            return false;
-        }
-
-        exitGates.put(n, new ExitGate(this, n));
-        System.out.println("Gate "+n+" added to Lot "+this.name+" as an Exit");
-
-        return true;
-    }
-
-    public boolean Enter(Car c, int n){
-        //make sure the gate exists
-        if(!entGates.containsKey(n)){
-            System.out.println("Invalid input, Lot "+this.name+" does not have an entrance gate "+n);
-            return false;
-        }
-        if(!entGates.get(n).EnterLot(c)){
+    public boolean Enter(Car c){
+        if(!entGate.EnterLot(c)){
             return false;
         }
         //allow the car to enter the lot
-        System.out.println("Car "+c.license+" enters Lot "+this.name+" through gate "+n);
+        System.out.println("Car "+c.license+" enters Lot "+this.name);
         //when a car enters the occupancy increases
         occupancy++;
         return true;
@@ -69,7 +44,7 @@ public class ParkingLot {
 
     public int getPrice(){
         //returns the price of the lot
-        return price;
+        return group.getPrice();
     }
 
     public void payTicket(int p){
@@ -77,19 +52,20 @@ public class ParkingLot {
         profit+=p;
     }
 
-    public boolean Exit(Car c, int n){
-        if(!exitGates.containsKey(n)){
-            System.out.println("Invalid input, Lot "+this.name+" does not have an exit gate "+n);
-            return false;
-        }
-        if(!exitGates.get(n).ExitLot(c)){
+    public boolean Exit(Car c){
+        if(!exitGate.ExitLot(c)){
             return false;
         }
         //the car exits the lot
-        System.out.println("Car "+c.license+" exits Lot "+this.name+" through gate "+n);
+        System.out.println("Car "+c.license+" exits Lot "+this.name);
         //the occupancy decreases
         occupancy--;
         return true;
+    }
+
+    public int inquireSpace(){
+        System.out.println("Parking Lot "+this.name+" sends information about current availability");
+        return (capacity-occupancy);
     }
 
 }
